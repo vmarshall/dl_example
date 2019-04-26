baf311f52d2d582e96d2befde11f90494fd7e3cb
darvin <darvin@sironamedical.com>
2019-04-25 22:29:08 -0700

    First full running AI pipeline.

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ Example code for a quick and dirty deep learning project.
 
 ## Necessary Packages
 `conda install pytorch torchvision cudastoolkit=9.0 -c pytorch`
+
 `conda install -c conda-forge pydicom`
+
+`conda install -c conda-forge itk`
 
--- utils/data_loaders.py
+++ utils/data_loaders.py
@@ -3,12 +3,16 @@ import os
 from os import listdir,mkdir,rmdir
 from os.path import join,isdir,isfile
 
+import pydicom
+import itk
 import numpy as np
+from skimage.transform import resize
 
 import torch
 from torch.utils.data import Dataset,DataLoader
 from torchvision import transforms,utils
 from torchvision.transforms import functional as func
+
 
 
 class RandomRotate(object):
@@ -28,6 +32,16 @@ class RandomFlip(object):
       X[:,:,:] = X[:,:,::-1]
     return {'X':X, 'Y':Y}
 
+class Resize(object):
+  def __init__(self, size=[256,256]):
+    self.size = size
+  def __call__(self, sample):
+    X,Y = sample['X'],sample['Y']
+    X_new = np.zeros((X.shape[0], self.size[0], self.size[1]))
+    for ii in range(X.shape[0]):
+      X_new[ii,:,:] = resize(X[ii,:,:], self.size, order=0, mode='constant', cval=0, anti_aliasing=True, preserve_range=True)
+    return {'X':X_new, 'Y':Y}
+
 class ToTensor(object):
   def __call__(self, sample):
     X,Y = sample['X'], sample['Y']
@@ -43,10 +57,117 @@ class ToTensor(object):
 
 
 
+def normalize_img(img):
+  img = img.astype(np.float32)
+  img -= np.min(img)
+  img /= (np.max(img) + 1e-6)
+  return img
+
+def read_in_dcm_acqnum(path_dcm):
+  dcm = pydicom.dcmread(path_dcm)
+  img = dcm.pixel_array
+  lab = (int(dcm.AcquisitionNumber) > 30) + 0
+  if len(img.shape) == 3:
+    img = img[:,:,0]
+  img = normalize_img(img)
+  return img,[lab]
+
+def read_in_dcm_gender(path_dcm):
+  dcm = pydicom.dcmread(path_dcm)
+
+  try:
+    PixelType = itk.ctype('signed short')
+    Dimension = 2
+    ImageType = itk.Image[PixelType, Dimension]
+    reader = itk.ImageSeriesReader[ImageType].New()
+    dicomIO = itk.GDCMImageIO.New()
+    reader.SetImageIO(dicomIO)
+    reader.SetFileNames([path_dcm])
+    reader.Update()
+    reader.GetOutput()
+    img = itk.GetArrayFromImage(reader.GetOutput())
+  except:
+    img = np.zeros((256,256))
+
+  lab = (dcm.PatientSex == 'M') + 0
+  if len(img.shape) == 3:
+    img = img[:,:,0]
+  img = normalize_img(img)
+  return img,[lab]
+
+class example_data_front(Dataset):
+  def __init__(self, path_data, ind=25, transform=None):
+    self.path_data = path_data
+    self.list_data = sorted(listdir(path_data))[:ind]
+    self.transform = transform
+  
+  def __len__(self):
+    return len(self.list_data)
+  
+  def __getitem__(self,idx):
+    path_dcm = join(self.path_data, self.list_data[idx])
+    X,Y = read_in_dcm_gender(path_dcm)
+    X = X.reshape([1, X.shape[0], X.shape[1]])
+    Y = np.array(Y).reshape([1, ])
+    sample = {'X': X, 'Y': Y}
+    if self.transform:
+      sample = self.transform(sample)
+    return sample
+
+class example_data_back(Dataset):
+  def __init__(self, path_data, ind=25, transform=None):
+    self.path_data = path_data
+    self.list_data = sorted(listdir(path_data))[ind:]
+    self.transform = transform
+  
+  def __len__(self):
+    return len(self.list_data)
+  
+  def __getitem__(self,idx):
+    path_dcm = join(self.path_data, self.list_data[idx])
+    X,Y = read_in_dcm_gender(path_dcm)
+    X = X.reshape([1, X.shape[0], X.shape[1]])
+    Y = np.array(Y).reshape([1, ])
+    sample = {'X': X, 'Y': Y}
+    if self.transform:
+      sample = self.transform(sample)
+    return sample
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class example_data(Dataset):
-  def __init__(self, path_data, transform=None):
+  def __init__(self, path_data,  transform=None):
     self.path_data = path_data
     self.transform = transform
   
